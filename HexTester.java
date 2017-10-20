f1cd7af27853f56ab5ef170378b2d772628af1ad
Chris <gatorman7654@gmail.com>
2017-10-20 13:18:47 -0400

    Add files via upload

--- HexTester.java
+++ HexTester.java
@@ -1,22 +1,21 @@
+/*
+TODO:
+
+1. implement two's complement for negative hex values
+2. problem with subHex
+
+*/
+
 public class HexTester{
 	public static void main(String args[]){
-		System.out.println(HexUtil.getHexValue(10));
-		System.out.println(HexUtil.getHexValue(11));
-		System.out.println(HexUtil.getHexValue(12));
-		System.out.println(HexUtil.getHexValue(13));
-		System.out.println(HexUtil.getHexValue(14));
-		System.out.println(HexUtil.getHexValue(15));
-		System.out.println(HexUtil.getHexValue(16));
-		System.out.println(HexUtil.getDecimalValue('A'));
-		System.out.println(HexUtil.getDecimalValue('B'));
-		System.out.println(HexUtil.getDecimalValue('C'));
-		System.out.println(HexUtil.getDecimalValue('D'));
-		System.out.println(HexUtil.getDecimalValue('E'));
-		System.out.println(HexUtil.getDecimalValue('F'));
-		System.out.println(HexUtil.getDecimalValue('1'));
-		System.out.println(HexUtil.getDecimalValue('2'));
-		System.out.println(HexUtil.getDecimalValue('3'));
-		System.out.println(HexUtil.getDecimalValue('4'));
+		
+		System.out.println(HexUtil.decimalToHex(256475, false));
+		System.out.println(HexUtil.addHex("AAA", "BBB"));
+		System.out.println(HexUtil.hexToDecimal("AAA"));
+		System.out.println(HexUtil.hexToDecimal("BBB"));
+		System.out.println(HexUtil.decimalToHex((HexUtil.hexToDecimal("AAA") - HexUtil.hexToDecimal("BBB")), true));
+		System.out.println(HexUtil.subHex("AAA", "AAA"));
+		
 		
 	}
 }
@@ -25,41 +24,47 @@ class HexUtil{
 
 	public static String addHex(String num1, String num2){
 		String result;
-		double doubleNum1 = hexToDecimal(num1);
-		double doubleNum2 = hexToDecimal(num2);
-		double resultdouble = doubleNum1 + doubleNum2;
-		result = decimalToHex(resultdouble);
+		boolean negative = false;
+		int doubleNum1 = hexToDecimal(num1);
+		int doubleNum2 = hexToDecimal(num2);
+		int resultdouble = doubleNum1 + doubleNum2;
+		if(resultdouble < 0)
+			negative = true;
+		result = decimalToHex(resultdouble, negative);
 		
 		return result;
 		
 	}
 
-	public static double hexToDecimal(String hex){
-		double result = 0;
-		double j = 0;
-		for(int i = hex.length()-1; i >= 0; i--){
-			result += (HexUtil.getHexValue(hex.charAt(i)) * Math.pow(16, j));
-			j++;
+	public static int hexToDecimal(String hex){
+		int result = 0;
+		int power = 0;
+		for(int i = hex.length() - 1; i >= 0; i--){
+			result += Character.getNumericValue(hex.charAt(i)) * Math.pow(16, power);
+			power++;
 		}
 		
 		return result;
 	}
 
-	public static String decimalToHex(double num){
+	public static String decimalToHex(int num, boolean negative){
 		String result;
 		StringBuilder resultBuilder = new StringBuilder();
-		double power = 0;
+		int power = 0;
 		
-		while(num / Math.pow(16, power) != 0){
+		while((int)(num / (Math.pow(16, power))) > 0){
 			
 			power++;
 			
 		}
-		
+		power--;
+		if(negative == true){
+			resultBuilder.append('-');
+		}
 		while(power >= 0){
 			
-			double currentVal = num / Math.pow(16,power);
-			num = num % Math.pow(16,power);
+			int currentVal = (int)(num / Math.pow(16,power));
+			num = (int)(num % Math.pow(16,power));
 			char nextChar = getHexValue(currentVal);
 			resultBuilder.append(nextChar);
 			power--;
@@ -116,10 +121,13 @@ class HexUtil{
 
 	public static String subHex(String num1, String num2){
 		String result;
-		double doubleNum1 = hexToDecimal(num1);
-		double doubleNum2 = hexToDecimal(num2);
-		double resultdouble = doubleNum1 - doubleNum2;
-		result = decimalToHex(resultdouble);
+		boolean negative= false;
+		int doubleNum1 = hexToDecimal(num1);
+		int doubleNum2 = hexToDecimal(num2);
+		int resultdouble = doubleNum1 - doubleNum2;
+		if(resultdouble < 0)
+			negative = true;
+		result = decimalToHex(resultdouble, negative);
 		return result;
 	}
 }
