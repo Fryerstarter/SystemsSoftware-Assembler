e76ea92251971079a0c0f402a65e6dd4852b659e
Chris <gatorman7654@gmail.com>
2017-10-31 20:40:58 -0400

    Add files via upload

--- HexTester.java
+++ HexTester.java
@@ -8,7 +8,11 @@ public class HexTester{
 		System.out.println(HexUtil.decimalToHex(-10000, true));
 		System.out.println(HexUtil.getDecimalValue('F'));
 		System.out.println(HexUtil.subHex("0006", "001A"));
-		System.out.println(HexUtil.buildMachineCode(1, 1, 0, 0, 1, 0, "14", "030", "0003", ""));
+		System.out.println(HexUtil.buildMachineCode(1, 1, 1, 0, "54", "36", "104E", "33", 3));
+		System.out.println(HexUtil.buildMachineCode(1, 1, 0, 0, "74", "FFFF", "020", "33", 3));
+		System.out.println(HexUtil.buildMachineCode(0, 1, 0, 0, "68", "0033", "0006", "33", 3));
+		System.out.println(HexUtil.buildMachineCode(1, 1, 0, 1, "48", "1036", "000A", "33", 4));
+		
 	}
 }
 
@@ -24,6 +28,20 @@ class HexUtil{
 		
 		return result;
 		
+	}
+	
+	//Takes two hex strings and subtracts them, returning a hex string
+	public static String subHex(String num1, String num2){
+		String result;
+		boolean negative = false;
+		long doubleNum1 = hexToDecimal(num1);
+		long doubleNum2 = hexToDecimal(num2);
+		long resultdouble = doubleNum1 - doubleNum2;
+		if (resultdouble < 0){
+			negative = true;
+		}
+		result = decimalToHex(resultdouble,negative);
+		return result;
 	}
 
 	//Takes a hex string and returns the decimal equivilent
@@ -77,55 +95,144 @@ class HexUtil{
 		return result;
 	}
 
-	//Takes two hex strings and subtracts them, returning a hex string
-	public static String subHex(String num1, String num2){
-		String result;
-		boolean negative = false;
-		long doubleNum1 = hexToDecimal(num1);
-		long doubleNum2 = hexToDecimal(num2);
-		long resultdouble = doubleNum1 - doubleNum2;
-		if (resultdouble < 0){
-			negative = true;
-		}
-		result = decimalToHex(resultdouble,negative);
-		return result;
-	}
 	
 	
-	
-	//need to change b,p handling. should not need in the argument. try p first then b if fails then throw fail code. 
-       
-	public static String buildMachineCode(int n, int i, int x, int b, int p, int e, String opCode, String memoryLoc, String LOCCTR, String BASE){
-		String machineCode;
-		String xbpeBinary;
-		String displacement = "";
+	public static String buildXBPE(int x, int b, int p, int e){
+		//returns the HEX representation of the XBPE half-byte
 		StringBuilder xbpeBinaryBuilder = new StringBuilder();
-		StringBuilder codeBuilder = new StringBuilder();
-		if(n ==1 && i == 0){
-			opCode = addHex(opCode, "2");
-		}else if(n == 0 && i == 1){
-			opCode = addHex(opCode, "1");
-		}else if(n ==1 && i == 1){
-			opCode = addHex(opCode, "3");
-		}
 		xbpeBinaryBuilder.append(x);
 		xbpeBinaryBuilder.append(b);
 		xbpeBinaryBuilder.append(p);
 		xbpeBinaryBuilder.append(e);
-		xbpeBinary = xbpeBinaryBuilder.toString();
-		int decimal = Integer.parseInt(xbpeBinary,2);
-		xbpeBinary = Integer.toString(decimal,16);
+		int inDecimal = Integer.parseInt(xbpeBinaryBuilder.toString(), 2);
+		
+		return Integer.toString(inDecimal, 16);
+		
+
+	}
+	
+	public static String buildOpNI(String op, int n, int i){
+		//builds the opcode half-byte
+		if(n == 0 && i == 1){
+			return addHex(op, "1");
+		}else if(n == 1 && i == 0){
+			return addHex(op, "2");
+		}else{
+			return addHex(op, "3");
+		}
+		
+		
+	}
+	
+	public static String formatDisplacement(String displacement, int format)
+	{
+		//pads the displacement with 0's to meet required length. supports format 3 and format 4 instructions.
+		int requiredLength = 0;
+		StringBuilder fDisp = new StringBuilder();
+		if(format == 3){
+			requiredLength = 3;
+			
+		}else if( format == 4){
+			requiredLength = 5;
+		}else{
+			return "-1";
+		}
+		for( int i = 0; i < requiredLength - displacement.length(); i++){
+			fDisp.append("0");
+		}
+		fDisp.append(displacement);
+		return fDisp.toString();
+		
+	}
+	
+	
+	public static String buildFormat2(String opCode, String r1, String r2){
+		StringBuilder codeBuilder = new StringBuilder();
 		codeBuilder.append(opCode);
-		codeBuilder.append(xbpeBinary);
+		codeBuilder.append(r1);
+		codeBuilder.append(r2);
+		
+		
+		return codeBuilder.toString().toUpperCase();
+	}
+	
+	public static String buildFormat3(int n, int i, int x, int e, String opCode, String memoryLoc, String LOCCTR, String BASE){
+		//returns -1 if unable to use base of pc relative
+		String machineCode;
+		int b = 0, p = 0;
+		
+		String displacement = "";
+		
+		StringBuilder codeBuilder = new StringBuilder();
+	
+		int pcDec = hexToDecimal(LOCCTR);
+		int memLocDec = hexToDecimal(memoryLoc);
+		int displacementDec = memLocDec - pcDec;
+		if(displacementDec <=2047 && displacementDec >= -2048){
+			b = 0;
+			p = 1;
+		}else{
+			displacementDec = memLocDec - hexToDecimal(BASE);
+			if( displacementDec>= 0 && displacementDec <= 4095){
+				b = 1;
+				p = 0;
+			}else{
+				
+				return "-1";
+			}
+		
+		}
+
+		codeBuilder.append(buildOpNI(opCode, n, i));
+		String xbpe = buildXBPE(x, b, p, e);
+		codeBuilder.append(xbpe);
 		
 		if(b == 0 && p == 1){
 			displacement = subHex(memoryLoc, LOCCTR);
 		}else if(b == 1 && p == 0){
 			displacement = subHex(memoryLoc, BASE);
 		}
-		codeBuilder.append(displacement);
-		machineCode = codeBuilder.toString();
-		return machineCode;
 		
+		
+		
+		codeBuilder.append(formatDisplacement(displacement, 3));
+		machineCode = codeBuilder.toString();
+		
+		return machineCode.toUpperCase();
+		
+	}
+	
+	public static String buildFormat4(int n, int i, int x, int e, String opCode, String memoryLoc){
+		int b = 0, p = 0;
+		StringBuilder codeBuilder = new StringBuilder();
+		
+		codeBuilder.append(buildOpNI(opCode, n, i));
+		codeBuilder.append(buildXBPE(x, b, p, e));
+		codeBuilder.append(formatDisplacement(memoryLoc, 4));
+		return codeBuilder.toString().toUpperCase();
+		
+	}
+	
+	
+	
+	
+		
+	
+       
+	public static String buildMachineCode(int n, int i, int x, int e, String opCode, String memoryLoc, String LOCCTR, String BASE, int format){
+		//returns -1 if unable to use base of pc relative
+		//format 2, for now, uses memoryLoc as r1 and BASE as r2
+		String machineCode = "-2";
+		if(format == 1){
+			machineCode = opCode;
+		}else if(format == 2){
+			machineCode = buildFormat2(opCode, memoryLoc, BASE);
+		}else if(format == 3){
+			machineCode = buildFormat3(n, i, x, e, opCode, memoryLoc, LOCCTR, BASE);
+		}else if(format == 4){
+			machineCode = buildFormat4(n, i, x, e, opCode, memoryLoc);
+		}
+		
+		return machineCode;
 	}
 }
